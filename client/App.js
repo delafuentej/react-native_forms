b42700b460aee3537d6fb4edee00a20ebe0b826d
delafuentej <jfuentelucas@gmail.com>
2024-05-31 19:02:08 +0200

    file/server folder added

---
+++ client/App.js
@@ -0,0 +1,144 @@
+import { StatusBar } from 'expo-status-bar';
+import useFormal from '@kevinwolf/formal-native';
+import React from 'react';
+import { StyleSheet, View, Alert, SafeAreaView} from 'react-native';
+import {Button, Input, Text} from 'react-native-elements';
+import * as yup from 'yup';
+
+
+const schema= yup.object().shape({
+
+    Username:yup
+      .string() // check if it a string
+      .required(),  //mandatory field
+  
+    Email: yup
+      .string()
+      .email('Invalid Email Address') //check if  it is a valid mail
+      .required(),
+    
+    Password: yup
+      .string()
+      .min(8) //min lenght caract.
+      .max(32) //max. lenght 32 carac
+      .required(),
+    
+    'Confirm Password': yup
+      .string()
+      .oneOf([yup.ref('Password'), null], 'Password do not match') // check if it matches with 'Password' or is null
+      .required(),
+      
+  })
+
+
+const initialValues = {
+  Username: '',
+  Email:'',
+  Password:'',
+  'Confirm Password': '',
+};
+
+
+const Field=({placeholder, error,...props})=>(
+
+      <>
+      <Text style={styles.space}>{placeholder}</Text>
+      <Input {...props}/>
+      {error && (
+          <Text style={[styles.space, error && styles.error]}>{error}</Text>
+      )}
+  
+      </>
+  
+    );
+
+
+export default function App() {
+  //useFormal hook wich takes in initialValues and a config object(wich contains the initial schema, wich makes sure the data that we have conforms with the schema)
+  // to send a POST request from de client side => server-side validation
+  const formal = useFormal(
+    initialValues,
+    {schema,
+    onSubmit: (values) => {
+      const username= values['Username']
+      const email= values['Email']
+      const password= values['Password']
+      const confirmPassword= values['Confirm Password']
+      fetch('http://localhost:3030/form', {
+        method: 'POST',
+        headers:{
+          Accept:'application/json',
+          'Content Type':'application/json',
+        },
+        body: JSON.stringify({ username, email, password, confirmPassword}),
+      })
+      .then(res => res.json())
+      //validation succesfully=> it alerts the data
+      .then( data =>{
+        Alert.alert(JSON.stringify(data));
+      })
+      //validation unsuccesfull<=> it alerts the error
+      .catch( error =>{
+        Alert.alert(JSON.stringify(error));
+      })
+    },
+});
+
+
+
+  return (
+    //SafeAreaView => makes sure our content renders within the safe area boundaries of a device
+    <SafeAreaView style={styles.container}>
+      <View style={styles.space}>
+        <Text h1>Formal</Text>
+
+        <>
+    <Text style={styles.space}>Username</Text>
+    <Input placeholder="Username" {...formal.getFieldProps('Username')} />
+    {formal.errors.Username && (
+      <Text style={[styles.space, formal.errors.Username && styles.error]}>{formal.errors.Username}</Text>
+    )}
+</>
+    
+          <Field {...formal.getFieldProps('Username')} placeholder='Username' />
+          <Field {...formal.getFieldProps('Email')} placeholder='Email' />
+          <Field 
+            {...formal.getFieldProps('Password')}
+            placeholder='Password'
+            secureTextEntry
+          />
+
+          <Field 
+            {...formal.getFieldProps('Confirm Password')}
+            placeholder='Confirm Password'
+            secureTextEntry
+          />
+
+        <Button
+          title='Submit'
+          style={styles.space}
+          // {...formal. getSubmitButtonProps()} =>this submet all the values stored in formal to the onSubmit method in useFormal hook config
+          //Only after pressing this button are the error messages show an the schema validated
+          {...formal.getSubmitButtonProps()}
+          disabled={false}
+        />
+
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  space: {
+      margin:10,
+  },
+  error:{
+    color:'red',
+  },
+});
---
+++ server/index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import{check, validationResult} from 'express-validator/check';
+
+
+// port
+
+const PORT=3030;
+//to initiate express
+const app= express();
+//middelware=> middleware functions have access to the req/ res object and the next function in the apps request-response cycle
+//middlewares allow intercept a request, to apply some logic, to send it to the next middleware in the request
+app.use(express.json());
+
+
+// second argument=>array=> it perfoms the server-side validation
+app.post('/form',[
+//it checks for the param 'username' wich comes from the client side. If it is empty the message is sent back to the client
+    check('username')
+        .not()
+        .isEmpty()
+        .withMessage('Username is a required field'),
+    check('email') 
+        .isEmail()
+        .withMessage('Invalid Email Address'),
+    check('password')
+        .isLenght({min:8})
+        .withMessage('Password must be at least 8 characters')
+        .isLenght({mas:32})
+        .withMessage('Password must be atmost 32 characters')
+        //custom method=> to check if the password and the confirmPass are the same
+        .custom((password, {req})=>{
+            if( password === req.body.confirmPass){
+                return password;
+            }else{
+                throw new Error('Password do not match')
+            }
+        }),
+],
+(req, res)=>{
+    //validationResult to dealing with errors. It takes in the req  object and returns an error to sent it back to the client
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({errors: errors.array()})
+    }// whent there are no errors, it takes 'username', 'email' and 'password' and create the user and send a message success back to the client
+    const {username, email, password} = req.body;
+    //todo create a  user with the above data
+    res.json({success:true, message:'User created successfully', user: {username, email}});
+},
+
+)
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on PORT: ${PORT}`)
+});
